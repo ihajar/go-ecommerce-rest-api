b740dc07cbae6374159bd174517247688ad933f4
macbook <macbook@MacBook-Air-ihajar.local>
2025-01-17 20:27:41 +0100

    commit 1

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,16 +1,20 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/ihajar/ecom-api/types"
+	"github.com/ihajar/ecom-api/utils"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -24,5 +28,21 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+
+	// get json payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+
+	// check if the user exists?
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
+
+	// create a user if it doesn't exit
+	// err := h.store.CreateUser
 
 }
